afc8e4e0985bf986128fd0a919a25b078c588863
Lauras5 <idlaurasalcido@gmail.com>
2021-03-27 22:22:07 -0700

    added weight for all planets because why not

--- calc_rust/src/main.rs
+++ calc_rust/src/main.rs
@@ -1,20 +1,32 @@
 use std::io::{self, Read};
+use std::collections::HashMap;
 
 fn main() {
-    println!("Enter your weight (kg): ");
+    // hold planet surface mass
+    let mut planets: HashMap<&str, f32>= HashMap::new();
+    planets.insert("Mercury", 0.38);
+    planets.insert("Venus", 0.91);
+    planets.insert("Earth", 1.0);
+    planets.insert("Mars", 0.38);
+    planets.insert("Jupiter", 2.34);
+    planets.insert("Saturn", 0.93);
+    planets.insert("Uranus", 0.92);
+    planets.insert("Neptune", 1.12);
+
+    println!("Enter your weight (lbs): ");
 
     let mut input = String::new();
     let stdin = io::stdin();
 
     stdin.read_line(&mut input);
 
-    let weight: f32 = input.trim().parse().unwrap();
-    let mars_weight: f32 = calculate_weight_on_mars(weight);
+    let mass: f32 = input.trim().parse().unwrap();
 
-    println!("Weight on Mars: {:.2}kg", mars_weight);
-}
-
-fn calculate_weight_on_mars(weight: f32) -> f32 {
-    (weight / 9.81) * 3.711
+    println!("\nYOUR WEIGHT ON DIFFERENT PLANETS IN OUR SOLAR SYSTEM");
+    println!("----------------------------------------------------");
+    for (key, value) in &planets {
+        let weight: f32 = &mass * value;
+        println!("{planet:<width$}{weight:>width$.2} lbs", weight=weight, planet=key, width=10);
+    }
 }
 
